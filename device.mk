27d01f4561f55e5acf1338b89acc518ed7191e57
Xenogenesys <xiulinfu333@gmail.com>
2025-02-28 22:22:45 +0100

    mondrian: Build MIUI camera

--- device.mk
+++ device.mk
@@ -10,6 +10,9 @@ $(call inherit-product, device/xiaomi/sm8450-common/common.mk)
 # Inherit from the proprietary version
 $(call inherit-product, vendor/xiaomi/mondrian/mondrian-vendor.mk)
 
+# Inherit from miuicamera-mondrian
+$(call inherit-product-if-exists, device/xiaomi/miuicamera-mondrian/device.mk)
+
 # Overlay
 PRODUCT_PACKAGES += \
     ApertureResMondrian \
